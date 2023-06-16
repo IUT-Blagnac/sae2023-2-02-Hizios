1c52cad8a3bdb5985f9c23628bf68c6dd063921a
Hizios <bsnclem@gmail.com>
2023-06-16 15:19:12 +0200

    algo 10

--- src/main/java/exercice/Main.java
+++ src/main/java/exercice/Main.java
@@ -4,9 +4,18 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        int i = 0;
         String texte = "exemple de texte a classer";
         List<Character> ordre = List.of('c', 't', 'e', 'x', 'a', 'm', 'p', 'l', 's', 'r', 'd');
+
+        long startTime = System.nanoTime();
         System.out.println(Exercice.solution(texte,ordre));
+        long endTime = System.nanoTime();
+        long durationInNanos = endTime - startTime;
+        double durationInMilliseconds = (double) durationInNanos / 1_000_000;
+
+        System.out.println("Durée en millisecondes : " + durationInMilliseconds);
+       
         // output: [classer, texte, exemple, a, de]
     }
 }
