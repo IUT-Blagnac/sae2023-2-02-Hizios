035ff3fb22315acce21c226897ae64048b02e81f
Hizios <bsnclem@gmail.com>
2023-06-16 15:52:15 +0200

    9

--- src/main/java/exercice/Exercice.java
+++ src/main/java/exercice/Exercice.java
@@ -1,127 +1,65 @@
 package exercice;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Exercice {
     public static List<String> solution(String str, List<Character> ordre) {
-        ArrayList<String> mots = stringToWords(str);
-        triFusion(mots, ordre);
-        return mots;
-    }
-
-    public static void triFusion(ArrayList<String> mots, List<Character> ordre) {
-        int taille = mots.size();
-    
-        if (taille <= 1) {
-            return;
-        }
-    
-        int mid = taille / 2;
-        ArrayList<String> left = new ArrayList<>(mots.subList(0, mid));
-        ArrayList<String> right = new ArrayList<>(mots.subList(mid, taille));
-    
-        triFusion(left, ordre);
-        triFusion(right, ordre);
-    
-        fusion(left, right, mots, ordre);
-    }
-    
-    public static void fusion(ArrayList<String> left, ArrayList<String> right, ArrayList<String> mots, List<Character> ordre) {
-        int tailleL = left.size();
-        int tailleR = right.size();
-        int i = 0, j = 0, k = 0;
-    
-        while (i < tailleL && j < tailleR) {
-            String leftI = left.get(i);
-            String rightJ = right.get(j);
-    
-            if (compare(leftI, rightJ, ordre) <= 0) {
-                mots.set(k, leftI);
-                i++;
-            } else {
-                mots.set(k, rightJ);
-                j++;
-            }
-            k++;
-        }
-    
-        while (i < tailleL) {
-            mots.set(k, left.get(i));
-            i++;
-            k++;
-        }
-    
-        while (j < tailleR) {
-            mots.set(k, right.get(j));
-            j++;
-            k++;
-        }
-    }
-
-    public static int compare (String chaine1, String chaine2, List<Character> ordre){
-        int position1;
-        int position2; 
-
-        if (chaine1.equals(chaine2)){
-            return 0;
-        }
-        if (chaine1.equals("")){
-            return -1;
-        }
-        if (chaine2.equals("")){
-            return 1;
-        }
-        if (chaine1.charAt(0)==(chaine2.charAt(0))){
-            return compare(chaine1.substring(1),chaine2.substring(1), ordre);
-        }
-
-        position1 = getPosition(ordre, chaine1.charAt(0));
-        position2 = getPosition(ordre, chaine2.charAt(0));
-        
-        if (position1 == -1 && position2 != -1){
-            return 1;
-        }
-        if (position2 == -1){
-            return -1;
-        }
-        if ( position1 > position2 ){
-            return 1;
-        }
-        else {
-            return -1;
-        }
-    }
-
-
-    public static int getPosition(List<Character> ordre, char lettre) {
-        return ordre.indexOf(lettre);
-    }
-
-    public static ArrayList<String> stringToWords(String str) {
-        ArrayList<String> sousChaines = new ArrayList<String>();
-        int startIndex = -1;
-    
-        for (int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
-    
-            if (Character.isLetterOrDigit(c)) {
-                if (startIndex == -1) {
-                    startIndex = i;
-                }
-            } else {
-                if (startIndex != -1) {
-                    String sousChaine = str.substring(startIndex, i);
-                    sousChaines.add(sousChaine);
-                    startIndex = -1;
+        // Liste des mots classés dans l'ordre
+        List<String> solution = new ArrayList<>();
+        // On vérifie si le string rentré est vide, cela permet d'éviter de perdre du temps à parcourir toutes les boucles si le string est vide
+        if (!str.isEmpty()) {
+            // Liste des mots séparés
+            List<String> stringToWordList = new ArrayList<>();
+            // Mot que l'on va append
+            String current = "";
+            // On parcours tout le string renseigné avec une for each
+            for (char c : str.toCharArray()) {
+                // Si le caractère courant est un espace ou un apostrophe on termine le mot et on l'ajoute à la liste
+                if (!isAlphanumeric(c)) {
+                    if(!current.isEmpty()){
+                        stringToWordList.add(current);
+                        current = "";
+                    }
+                } else {
+                    current += c;
                 }
             }
+            // On ajoute le dernier mot à la liste
+            stringToWordList.add(current);
+            // On crée une nouvelle liste qui contient en premier lieu tous les mots non classés
+            List<String> unsorted = new ArrayList<>();
+            unsorted.addAll(stringToWordList);
+            System.out.println(stringToWordList);
+            // On parcours tous les caractères de l'ordre donné
+            for (char c : ordre) {
+                // on parcours tous les mots de la liste
+                for (String s : stringToWordList) {
+                    // Si le premier caractère du mot correspond alors on l'ajoute et on le retire de la liste des non classés pour ne finir qu'avec ceux qui ne le sont pas
+                    if (s.charAt(0) == c) {
+                        solution.add(s);
+                        unsorted.remove(s);
+                    }
+                }
+            }
+            // On ajoute à la fin tous les mots non classés dans l'ordre dans lequel ils sont arrivés dans la liste de mots initiale
+            solution.addAll(unsorted);
+
+
+
         }
-    
-        if (startIndex != -1) {
-            String sousChaine = str.substring(startIndex);
-            sousChaines.add(sousChaine);
+        // Si le string de base est vide on renvoie simplement un liste vide.
+        else{
+            solution = List.of();
         }
-    
-        return sousChaines;
+
+        return solution;
+    }
+
+
+
+    // Méthode permettant de vérifier si un caractère est alphanumérique
+    private static boolean isAlphanumeric(char c){
+        return (Character.isLetter(c) || Character.isDigit(c));
     }
 }
